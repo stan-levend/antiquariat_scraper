3cf056cc29a93e0a6d34c3961b0ae075096ce782
stan-levend <s.levendovsky@gmail.com>
2021-07-29 12:27:59 +0200

    added Paseka antiquart, updated output formatting of antiquart

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # Antiquart book scraper
 
-This scraper uses <code>BeautifulSoup</code> and <code> requests</code>  to scrape data from multiple antiqart book shops located in Kosice, Slovakia. The output will list the query results that consist of the query in form of an author of book (book title) and price for the book.  
+This scraper uses <code>BeautifulSoup</code> and <code>requests</code>  to scrape data from multiple antiqart book shops located in Kosice, Slovakia. The output will list the query results that consist of the query in form of an author of book (book title) and price for the book.
 
 Currently added sites to scrape:
 * [Antikvart](https://www.antikvart.sk/)
 * [Antiqart](https://www.antiqart.sk/)
-## How To Use 
+* [Paseka](http://www.paseka.sk/index.php?)
+### How To Use
 
 At the terminal, type:
 ```
 python3 main.py
-``` 
+```
 which will execute the main script that read queries from <code>queries.txt</code>.
 
 ### <code>queries.txt</code>  example input:
@@ -20,9 +21,18 @@ chesterton, nesbo, chiang, tolkien
 #dostojevski, orwell
 ```
 will only add first line to the list of queries.
+Use `#` at the start of the line to exclude the line from the list of queries.
 
 ## Things to add in the future:
-- [ ] Add other sites to scrape.
+- Add other sites to scrape.
+    - [x] http://www.paseka.sk/index.php?
+    - [ ] https://www.wallstreet.sk/antique.php
+    - [ ] https://www.pokladyzpovaly.sk/
+    - [ ] https://www.usialenca.sk/
+    - [ ] https://www.antikvariatshop.sk/
+    - [ ] https://www.antikvariatik.sk/?
+
+- [ ] Divide shops into separate `.py` files
 - [x] Add txt file for queries input.
 - [ ] Take input directly from command line.
 - [ ] Make GUI.
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def antikvart(query):
   soup = BeautifulSoup(req.content, 'html.parser')
   print(query.upper())
 
-  print('ANTIKVART \nPAGE 1 \n')
+  print('------------------ANTIKVART------------------\nPAGE 1 \n')
   search_antikvart_page(soup)
   print('\n')
 
@@ -29,61 +29,110 @@ def antikvart(query):
       soup = BeautifulSoup(req.content, 'html.parser')
       print(f'PAGE {i}\n')
       search_antikvart_page(soup)
-      
+
 def search_antikvart_page(soup):
   for book in soup.find_all("div", class_="tc_popis"):
+    print(book.h2.a.get_text())
     print(book.h3.a.get_text())
-    print(book.find('strong').get_text())
-    print(book.h2.a.get_text(), '\n')
+    print(book.text.splitlines()[3].strip())
+    print(book.find('strong').get_text(), '\n')
+
 
 def antiqart(query):
   url = f"https://www.antiqart.sk/search/node/{query.strip().lower().replace(' ','%20')}"
   req = requests.get(url)
   soup = BeautifulSoup(req.content, 'html.parser')
-  
-  print('ANTIQART \nPAGE 1\n')
-  search_antiqart_page(soup)
+
+  print('------------------ANTIQART------------------\nPAGE 1')
+  search_antiqart_page(soup, url)
 
   #find if query request got more than 1 page, if not, continue to another author
   pages = soup.find('li', class_='pager-last')
-  if(pages is None): 
-    print('______________________')
+  if(pages is None):
+    print('____________________________________________')
     return
   else:
-    n_pages = re.findall(r'\d+', pages.a.get('href'))[-1] #regex to find number (int) of pages 
+    n_pages = int(re.findall(r'\d+', pages.a.get('href'))[-1]) #regex to find number (int) of pages
     #browse through every page
-    for i in range(1, int(n_pages) + 1):
+    for i in range(1, n_pages + 1):
       url = f"https://www.antiqart.sk/search/node/{query.strip().lower().replace(' ','%20')}?page={i}"
       req = requests.get(url)
       soup = BeautifulSoup(req.content, 'html.parser')
-      print(f'PAGE {i+1}\n')
-      search_antiqart_page(soup)
-      
-    print('______________________')
+      print(f'PAGE {i+1}')
+      search_antiqart_page(soup, url)
 
-def search_antiqart_page(soup):
+    # print('____________________________________________')
+
+def search_antiqart_page(soup, url):
+  print(url, '\n')
   for book in soup.find_all("li", class_="search-result"):
     search = book.div.p.get_text()
     if 'Stav skladu:' in search:
-      print(book.h3.a.get_text())
-      print(search.strip(), '\n')
+      title = book.h3.a.get_text()
+      print(title)
+      items = search.split()
+      if items[0] == '...':
+        print(" ".join(items[4:-8]), '\n')
+      else:
+        title_len = len(title.split())
+        trim_size = title_len + 3
+        print(" ".join(items[trim_size:-7]), '\n')
+
+
+
+
+def paseka(query):
+  url = f"http://www.paseka.sk/index.php?&s_text={query.strip().lower().replace(' ','%20')}&order=timeof&page=katalog"
+  req = requests.get(url)
+  soup = BeautifulSoup(req.content, 'html.parser')
+
+  print('------------------PASEKA------------------\nPAGE 1\n')
+  search_paseka_page(soup)
+
+  #find if query request got more than 1 page, if not, continue to another author
+  pages = soup.find('p', class_='pages')
+  if(pages is None):
+    # print('______________________')
+    return
+  else:
+    n_pages = int(re.findall(r'\d+', pages.get_text())[1])
+    #browse through every page
+    for i in range(2, n_pages + 1):
+      url = f"http://www.paseka.sk/index.php?&s_text={query.strip().lower().replace(' ','%20')}&pg={i}&order=timeof&page=katalog"
+      req = requests.get(url)
+      soup = BeautifulSoup(req.content, 'html.parser')
+      print(f'PAGE {i}\n')
+      search_paseka_page(soup)
+
+    print('____________________________________________')
+
+def search_paseka_page(soup):
+  for book in soup.find_all("div", class_="book-data"):
+    print(book.h3.a.get_text())
+    print(book.div.a.get_text())
+    print(book.div.find("span", class_='data').get_text())
+    print(book.div.div.a.span.get_text(), '\n')
+
 
 if __name__ == "__main__":
   queries = [
+  # 'jules verne'
+  'doyle'
   # 'chesterton', 'nesbo', 'chiang', 'tolkien', 'rovelli', 'tolstoj',
   # 'lewis', 'dostojevski', 'orwell', 'eldredge', 'tegmark', 'peterson', 'doyle', 'asimov', 'feynman', 'eriksen', ''
   # 'rovelli', 'smolin', 'hawking', 'tyson', 'eriksen', 'wheeler archibald'
   ]
 
-  with open('queries.txt', 'r') as fd:
-      reader = csv.reader(fd)
-      for row in reader:
-        if row[0][0] == '#':
-          continue
-        # print(row)
-        queries.extend(row)
-  
+  # with open('queries.txt', 'r') as fd:
+  #     reader = csv.reader(fd)
+  #     for row in reader:
+  #       if row[0][0] == '#':
+  #         continue
+  #       # print(row)
+  #       queries.extend(row)
+
   for query in queries:
     antikvart(query)
     antiqart(query)
+    paseka(query)
     # pass
